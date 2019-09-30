8ffae7bdf0156d74799b489b5e1bbaaa8902c35a
GG Guitart <gertaguitart2019@u.northwestern.edu>
2019-09-30 14:46:21 -0500

    first upload

---
+++ keys.js
@@ -0,0 +1,7 @@
+console.log('this is loaded');
+
+exports.spotify = {
+    //initializing spotify ID
+  id: process.env.eaf7461e977c49cb981372e341de0872,
+  secret: process.env.ff085806f0543c195873abb5b9e78c4,
+};
